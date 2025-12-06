82ee9c44b5d6824f32cd654008f8d4ed5ed6bd66
Vladislav B <omix60441@gmail.com>
2025-12-06 17:24:19 +0200

    Добавляем API метод для получения данных по выбрнному movie элементу

---
+++ GMBackend/GM.Contracts/Queries/Movies/MovieQueryResponse.cs
@@ -0,0 +1,8 @@
+﻿using GM.Contracts.Models;
+
+namespace GM.Contracts.Queries.Movies;
+
+public class MovieQueryResponse : IQueryResponse
+{
+    public MovieModel Movie { get; set; }
+}
---
+++ GMBackend/GM.WebApi/Responses/MovieResponse.cs
@@ -0,0 +1,8 @@
+﻿using GM.Contracts.Models;
+
+namespace GM.WebApi.Responses;
+
+public class MovieResponse
+{
+    public MovieModel Movie { get; set; }
+}
