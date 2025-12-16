0d7d253bb4420b99959a9b6c655a5577fe621e7c
Vladislav B <omix60441@gmail.com>
2025-12-16 21:22:23 +0200

    Добавляем в проект с обработчиками сообщений, тестовый обработчик и DI модуль

---
+++ GMBackend/GM.MessagingHandlers/Handlers/TestRabbitQueueHandler.cs
@@ -0,0 +1,16 @@
+﻿using GM.RabbitMessaging.Contracts;
+using GM.RabbitMessaging.Queues.Interfaces;
+using NLog;
+
+namespace GM.MessagingHandlers.Handlers;
+
+public class TestRabbitQueueHandler : IQueueMessageHandler<TestRabbitQueue>
+{
+    private readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+    public Task HandleAsync(TestRabbitQueue message, CancellationToken cancellation)
+    {
+        this.logger.Trace("Test message received...");
+        return Task.CompletedTask;
+    }
+}
