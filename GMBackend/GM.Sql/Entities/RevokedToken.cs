6faabd08d6583d1c6f595c545525443d0d78ca4b
Vladislav B <omix60441@gmail.com>
2026-01-21 23:42:19 +0200

    Поправляем работу с БД (кофнфигурации сущностей)

--- GMBackend/GM.Sql/Entities/RevokedToken.cs
+++ GMBackend/GM.Sql/Entities/RevokedToken.cs
@@ -1,7 +1,9 @@
 namespace GM.Sql.Entities;
 
-public class ApplicationToken
+public class RevokedToken
 {
+    public long Id { get; set; }
     public string ApplicationName { get; set; }
     public DateTime Expire { get; set; }
+    public string Token { get; set; }
 }
